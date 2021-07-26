cd0dc7ffe94ac38aafd446ab6c86bd82d3c674e4
Wayne Chan <wchan.it@outlook.com>
2021-07-25 21:09:16 -0500

    initial commit

---
+++ src/main/java/com/hoaxify/HoaxifyApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hoaxify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoaxifyApiBackendApplication.class, args);
+    }
+
+}
